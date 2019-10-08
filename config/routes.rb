c59db6f62dcfed28c3f3f6d0504adace55facfce
Rio3zaky <misarakotoniaina@yahoo.fr>
2019-10-08 14:18:49 -0400

    first front end commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#index'
+  get '/form', to: 'pages#form'
+  get '/residential', to: 'pages#residential'
+  get '/commercial', to: 'pages#commercial'
+
 end
