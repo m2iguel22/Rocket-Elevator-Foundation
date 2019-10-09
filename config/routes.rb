c0c0dc0d4157c535e4b2d99b92948d8fa3f18717
Stevedt <steve.d.toutant@gmail.com>
2019-10-09 18:07:16 -0400

    add login rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admins
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#index'
   get '/form', to: 'pages#form'
   get '/residential', to: 'pages#residential'
   get '/commercial', to: 'pages#commercial'
-
+  get 'home/index'
   resources :employees
 end
