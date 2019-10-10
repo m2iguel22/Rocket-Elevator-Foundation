f1c069d40616dc2d70ae85cfead11020714607cd
JonathanFortinDominguez <jonathanfdominguez@gmail.com>
2019-10-10 09:56:14 -0400

    made changes to fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#index'
   get '/form', to: 'pages#form'
   get '/residential', to: 'pages#residential'
   get '/commercial', to: 'pages#commercial'
-
+  get 'home/index'
+  get '/users/sign_out', to: 'pages#index'
   resources :employees
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_221628) do
+ActiveRecord::Schema.define(version: 2019_10_10_014250) do
+
+  create_table "Administrators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "full_name"
+    t.string "company"
+    t.string "email"
+    t.integer "phone"
+    t.string "project_name"
+    t.string "department_in_charge"
+    t.text "project"
+    t.text "message"
+    t.string "username"
+    t.string "password"
+  end
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -45,16 +58,15 @@ ActiveRecord::Schema.define(version: 2019_10_07_221628) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "full_name"
-    t.string "company"
-    t.string "email"
-    t.integer "phone"
-    t.string "project_name"
-    t.string "department_in_charge"
-    t.text "project"
-    t.text "message"
-    t.string "username"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
