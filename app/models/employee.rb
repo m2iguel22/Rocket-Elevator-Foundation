5342459e1b117b89e3f37a8af4c1729d603596d6
Stevedt <steve.d.toutant@gmail.com>
2019-10-10 14:55:53 -0400

    user database randomly generated 4500

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,12 @@
 class Employee < ApplicationRecord
+    create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+        t.integer "user_id"
+        t.string "name"
+        t.string "first_name"
+        t.string "occupation_profession"
+        t.string "email_adress"
+        t.integer "token"
+        t.string "user_name"
+        t.string "password"
+      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_014250) do
+ActiveRecord::Schema.define(version: 2019_10_10_164002) do
 
   create_table "Administrators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "full_name"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2019_10_10_014250) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "clients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -71,7 +81,10 @@ ActiveRecord::Schema.define(version: 2019_10_10_014250) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "age"
     t.string "email", default: "", null: false
+    t.string "phone_number"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
