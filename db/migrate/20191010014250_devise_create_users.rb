dee8bd054bc67f392ce0e0eaab63dcbe2fb57a42
JonathanFortinDominguez <jonathanfdominguez@gmail.com>
2019-10-11 13:22:51 -0400

    fixed double devise, added admins to users, added employees tables and fixed login/logout

--- db/migrate/20191010014250_devise_create_users.rb
+++ db/migrate/20191010014250_devise_create_users.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-     
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+    
+      t.string :email            
+      t.string :encrypted_password
 
       ## Recoverable
       t.string   :reset_password_token
