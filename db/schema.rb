e5f761ad9623543334bd2807775f1ffab0fa4298
Rio3zaky <misarakotoniaina@yahoo.fr>
2019-10-11 20:37:47 -0400

    fixed some errors with tables in admin panel

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_10_11_141952) do
     t.string "title"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_employees_on_email", unique: true
