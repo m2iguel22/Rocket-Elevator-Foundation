cf31ae00d6a289400aa2e85213e597c3961ecfd1
JonathanFortinDominguez <54863706+JonathanFortinDominguez@users.noreply.github.com>
2019-10-18 23:00:01 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  include RailsAdminCharts
+
    devise :database_authenticatable, :registerable,
          :rememberable, :validatable, :recoverable  
   has_one :customer
