e5c7349ef7ed6f61bb8fbd667ea713cec74d0262
JonathanFortinDominguez <54863706+JonathanFortinDominguez@users.noreply.github.com>
2019-10-18 22:59:32 -0400

    Update employee.rb

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee< ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  include RailsAdminCharts
+  
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable  
   has_many :batteries, dependent: :destroy
