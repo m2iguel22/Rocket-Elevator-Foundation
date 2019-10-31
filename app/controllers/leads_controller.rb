a730ca6ac17230c5873dc48f8b18d31c2627ffe3
gbibeau1 <gabriel_bibeau@hotmail.com>
2019-10-31 14:55:04 -0400

    deploy pp

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -18,54 +18,43 @@ class LeadsController < ApplicationController
         params_attach = params['Attachment']
 
         
-        # @lead.attachment.attach(params['Attachment']) 
-
-        # comment = Comment.create! params.require(:comment).permit(:content)
-        # @lead.attachment.attach(params['Attachment'])
-        # @lead.attachment = params_attach.original_filename
-        # dropbox_client = DropboxApi::Client.new(ENV['DROPBOX_OAUTH_BEARER'])
-        # file_content = params['Attachment']
-        # dropbox_client.upload(@lead.image, file_content)
-        # result = client.list_folder "/Rocket"
-
+        
         if params_attach
             @lead.attachment = params_attach.read
             @lead.original_filename = params_attach.original_filename
-        
-        # redirect_to comment  
-        
-        # if verify_recaptcha(model: @lead)
-        #     @lead.save!
-        
-        
-        # else
-        #     redirect_to root_path
-        # end
+        end
+       
 
         
             puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
             
             #   if @lead.attachment != nil
-            client = DropboxApi::Client.new(ENV['DROPBOX_OAUTH_BEARER'])
-                # result = client.list_folder ""
-                # result.entries
-                # result.has_more?
-                # binary_content = IO.binread(bob).path
-                # client.create_folder("/#{@lead.full_name}")
-                # file_content = @lead.attachment.read @lead.attachment
-                # puts "??????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????"
-                # client.upload("/#{@lead.full_name}/#{File.basename(bob_name)}", bob)
-                # puts "TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTS"
-                # if client.search("/#{self.full_name}/")
-                #   lead.attachment = nil
-                # #   lead.original_file_name = nil
-                #   lead.save!
-                #   end
+            # client = DropboxApi::Client.new(ENV['DROPBOX_OAUTH_BEARER'])
+                
                 
               
+        if @lead.save!
+        redirect_to quote_confirm_path
+        else
+            redirect_to root
+        end
+        # if params_attach
+        #     asdf = params_attach.read
+        #     asdf_name = params_attach.original_filename
+        # end
+       
+
+        # client = DropboxApi::Client.new(ENV['DROPBOX_OAUTH_BEARER'])
+        #     client.create_folder("/#{@lead.full_name}")
+        #     client.upload("/#{@lead.full_name}/#{File.basename(asdf_name)}", asdf)
+        #     puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
             
-              @lead.save!
-              redirect_to quote_confirm_path
-    end
+        #     #   if @lead.attachment != nil
+        #     # client = DropboxApi::Client.new(ENV['DROPBOX_OAUTH_BEARER'])
+                
+                
+              
+        #  @lead.save!
+        # redirect_to quote_confirm_path
 end
 end
