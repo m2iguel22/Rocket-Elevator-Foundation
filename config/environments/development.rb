bf4f121d76700cb4c84e4cfa67e3bcd2ca763862
gbibeau1 <gabriel_bibeau@hotmail.com>
2019-11-04 15:24:44 -0500

    create tables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.default_url_option = { host: 'rocketgb.best'}  
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
